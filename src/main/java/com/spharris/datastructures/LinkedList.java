69093af5892c18e716fa7dc02a6c055599cbd3c8
Scott Harris <sharris@genability.com>
2015-12-13 21:31:03 -0800

    Add and insert

--- src/main/java/com/spharris/datastructures/LinkedList.java
+++ src/main/java/com/spharris/datastructures/LinkedList.java
@@ -1,12 +1,48 @@
 package com.spharris.datastructures;
 
+/**
+ * A doubly-linked list
+ */
 public class LinkedList<T> {
+	
+	private Node<T> first;
+	private Node<T> last;
+	
+	private int size = 0;
+	
+	/**
+	 * Adds an item to the end of the list
+	 */
 	public void add(T item) {
+		if (first == null) {
+			first = new Node<>(item, null, null);
+			last = first;
+		} else {
+			Node<T> newNode = new Node<>(item, null, last);
+			last.setNext(newNode);
+			last = newNode;
+		}
 		
+		size++;
 	}
 	
 	public void insert(int index, T item) {
+		if (index > size) {
+			throw new IndexOutOfBoundsException();
+		}
 		
+		if (index == size) {
+			Node<T> node = last;
+			Node<T> newNode = new Node<>(item, node, null);
+			node.setNext(newNode);
+		} else {
+			Node<T> node = getNode(index);
+			Node<T> newNode = new Node<>(item, node.getPrev(), node);
+			node.getPrev().setNext(newNode);
+			node.setPrev(newNode);
+		}
+		
+		size++;
 	}
 	
 	public T remove(int index) {
@@ -18,7 +54,8 @@ public class LinkedList<T> {
 	}
 	
 	public T get(int index) {
-		return null;
+		Node<T> node = getNode(index);
+		return node.getData();
 	}
 	
 	public boolean contains(T item) {
@@ -26,10 +63,59 @@ public class LinkedList<T> {
 	}
 	
 	public int size() {
-		return 0;
+		return size;
 	}
 	
 	public boolean isEmpty() {
-		return true;
+		return size == 0;
+	}
+	
+	private Node<T> getNode(int index) {
+		if (index > (size - 1)) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		int currentIndex = 0;
+		Node<T> currentNode = first;
+		while (currentIndex != index) {
+			currentNode = currentNode.getNext();
+			currentIndex++;
+		}
+		
+		return currentNode;
+	}
+	
+	private static class Node<T> {
+		
+		private T data;
+		
+		private Node<T> next;
+		private Node<T> prev;
+		
+		private Node(T data, Node<T> next, Node<T> prev) {
+			this.data = data;
+			this.next = next;
+			this.prev = prev;
+		}
+		
+		public Node<T> getNext() {
+			return next;
+		}
+		
+		public void setNext(Node<T> node) {
+			this.next = node;
+		}
+		
+		public Node<T> getPrev() {
+			return prev;
+		}
+		
+		public void setPrev(Node<T> node) {
+			this.prev = node;
+		}
+		
+		public T getData() {
+			return data;
+		}
 	}
 }
--- src/test/java/com/spharris/datastructures/TestLinkedList.java
+++ src/test/java/com/spharris/datastructures/TestLinkedList.java
@@ -1,8 +1,137 @@
 package com.spharris.datastructures;
 
 import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import static org.hamcrest.Matchers.*;
 
 public class TestLinkedList {
-
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void isEmptyReturnsTrueForEmptyList() {
+		// Arrange
+		LinkedList<Object> l = new LinkedList<>();
+		
+		// Act
+		// None
+		
+		// Assert
+		assertThat(l.isEmpty(), equalTo(true));
+	}
+	
+	@Test
+	public void emptyListHasZeroSize() {
+		// Arrange
+		LinkedList<Object> l = new LinkedList<Object>();
+		
+		// Act
+		// None
+		
+		// Assert
+		assertThat(l.size(), equalTo(0));
+	}
+	
+	@Test
+	public void addOneItem() {
+		LinkedList<Integer> l = new LinkedList<>();
+		
+		l.add(5);
+		
+		assertThat(l.size(), equalTo(1));
+		assertThat(l.isEmpty(), equalTo(false));
+	}
+	
+	@Test
+	public void getAnItem() {
+		LinkedList<Integer> l = new LinkedList<>();
+		
+		l.add(5);
+		
+		assertThat(l.get(0), equalTo(5));
+	}
+	
+	@Test
+	public void getAnItemThatDoesntExist() {
+		LinkedList<Integer> l = new LinkedList<>();
+		
+		thrown.expect(IndexOutOfBoundsException.class);
+		
+		l.get(0);
+	}
+	
+	@Test
+	public void getSecondItem() {
+		LinkedList<Integer> l = new LinkedList<>();
+		
+		l.add(5);
+		l.add(6);
+		
+		assertThat(l.get(0), equalTo(5));
+		assertThat(l.get(1), equalTo(6));
+	}
+	
+	@Test
+	public void addManyItems() {
+		LinkedList<Integer> l = new LinkedList<>();
+		
+		int numItems = 10;
+		for (int i = 0; i < numItems; i++) {
+			l.add(i);
+		}
+		
+		assertThat(l.size(), equalTo(numItems));
+		for (int i = 0; i < numItems; i++) {
+			assertThat(l.get(i), equalTo(i));
+		}
+	}
+	
+	@Test
+	public void insert() {
+		LinkedList<Integer> l = new LinkedList<>();
+		
+		int numItems = 10;
+		for (int i = 0; i < numItems; i++) {
+			l.add(i);
+		}
+		
+		l.insert(4, 16);
+		assertThat(l.size(), equalTo(11));
+		assertThat(l.get(4), equalTo(16));
+	}
+	
+	@Test
+	public void insertAfterEndThrowsException() {
+		LinkedList<Integer> l = new LinkedList<>();
+		
+		int numItems = 10;
+		for (int i = 0; i < numItems; i++) {
+			l.add(i);
+		}
+		
+		thrown.expect(IndexOutOfBoundsException.class);
+		
+		l.insert(15, 10);
+	}
+	
+	@Test
+	public void insertAtEnd() {
+		LinkedList<Integer> l = new LinkedList<>();
+		
+		int numItems = 10;
+		for (int i = 0; i < numItems; i++) {
+			l.add(i);
+		}
+		
+		l.insert(numItems, numItems);
+		assertThat(l.size(), equalTo(11));
+		for (int i = 0; i < numItems + 1; i++) {
+			assertThat(l.get(i), equalTo(i));
+		}
+	}
 }
