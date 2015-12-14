1a9d1ee4e918d92cd8c54d49f783772a9345338d
Scott Harris <spharris@gmail.com>
2015-12-13 20:53:29 -0800

    First commit

---
+++ src/main/java/com/spharris/datastructures/LinkedList.java
@@ -0,0 +1,35 @@
+package com.spharris.datastructures;
+
+public class LinkedList<T> {
+	public void add(T item) {
+		
+	}
+	
+	public void insert(int index, T item) {
+		
+	}
+	
+	public T remove(int index) {
+		return null;
+	}
+	
+	public T remove(T item) {
+		return null;
+	}
+	
+	public T get(int index) {
+		return null;
+	}
+	
+	public boolean contains(T item) {
+		return false;
+	}
+	
+	public int size() {
+		return 0;
+	}
+	
+	public boolean isEmpty() {
+		return true;
+	}
+}
---
+++ src/test/java/com/spharris/datastructures/TestLinkedList.java
@@ -0,0 +1,8 @@
+package com.spharris.datastructures;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+public class TestLinkedList {
+
+}
